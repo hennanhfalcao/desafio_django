0bbedfb1e4ae09397fa8f04cb9b03851488a4167
Hennan H. Falcão <hennan.falcao@cs.unipe.edu.br>
2024-11-22 23:47:49 -0300

    :bug: bugfix: correcao de bug

--- api/signals.py
+++ api/signals.py
@@ -1,16 +1,13 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.apps import apps
 from django.contrib.auth.models import User
+from api.models import ModelUserProfile
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile = apps.get_model('api', 'UserProfile')  # Obtém o modelo dinamicamente
-        UserProfile.objects.create(user=instance)
-
+        ModelUserProfile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    UserProfile = apps.get_model('api', 'UserProfile')  # Obtém o modelo dinamicamente
     instance.profile.save()
