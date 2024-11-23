b7ab8bcbc79b90b2beeda92fe61759331bb3a579
Hennan H. Falcão <hennan.falcao@cs.unipe.edu.br>
2024-11-23 14:42:16 -0300

    :sparkles: feat: endpoints referentes as respostas implementados

--- api/views.py
+++ api/views.py
@@ -4,6 +4,8 @@ from api.routers.exam import router as exam_router
 from api.routers.question import router as question_router
 from api.routers.choice import router as choice_router
 from api.routers.participation import router as participation_router
+from api.routers.answer import router as answer_router
+
 
 api = NinjaAPI()
 
@@ -12,3 +14,4 @@ api.add_router("/exams", exam_router)
 api.add_router("/questions", question_router)
 api.add_router("/choices", choice_router)
 api.add_router("/participations", participation_router)
+api.add_router("/answers", answer_router)
