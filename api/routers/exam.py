e743c0238f3878e31d96cd1f9c650d81eb9f9481
Hennan H. Falcão <hennan.falcao@cs.unipe.edu.br>
2024-11-25 13:47:44 -0300

    :sparkles: feat: implementação das operações de put, patch e delete no router de provas.

--- api/routers/exam.py
+++ api/routers/exam.py
@@ -23,3 +23,53 @@ def list_exams(request, query: str = None, order_by: str = "-created_at", page: 
     start = (page-1)*page_size
     end = start + page_size
     return exams[start:end]
+
+@router.get("/{exam_id}/", response=ExamSchema)
+def get_exam(request, exam_id: int):
+    try:
+        exam = ModelExam.objects.get(id=exam_id)
+    except ModelExam.DoesNotExist:
+        return {"detail":"Exam not found"}, 404
+    return ExamSchema.from_orm(exam)
+
+@router.put("/{exam_id}/", response=ExamSchema)
+def update_exam(request, exam_id:int, data: ExamCreateSchema):
+    if not request.user.is_authenticated:
+        return {"detail": "Authentication required"}, 401
+    
+    try:
+        exam = ModelExam.objects.get(id=exam_id)
+    except ModelExam.DoesNotExist:
+        return {"detail":"Exam not found"}, 404
+
+    exam.name = data.name
+    exam.save()
+    return ExamSchema.from_orm(exam)    
+
+@router.patch("/{exam_id}/", response=ExamSchema)
+def partial_update_exam(request, exam_id: int, data: ExamCreateSchema):
+    if not request.user.is_authenticated:
+        return {"detail":"Authentication required"}, 401
+    
+    try:
+        exam= ModelExam.objects.get(id=exam_id)
+    except ModelExam.DoesNotExist:
+        return {"detail":"Exam not found"}, 404
+
+    if data.name is not None:
+        exam.name = data.name
+    exam.save()
+    return ExamSchema.from_orm(exam)
+
+@router.delete("/{exam_id}/", response={204: None})
+def delete_exam(request, exam_id:int):
+    if not request.user.is_authenticated:
+        return {"detail":"Authentication required"}, 401
+    
+    try:
+        exam = ModelExam.objects.get(id=exam_id)
+    except ModelExam.DoesNotExist:
+        return {"detail": "Exam not found"}, 404
+
+    exam.delete()
+    return 204, None    
